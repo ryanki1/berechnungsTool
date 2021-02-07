0f0432ffc96607b7c7b515f2ea96eedfe55a03b6
Kieran Ryan <contactkieran@hotmail.com>
2021-02-07 23:59:10 +0100

    9. Additional information in the tool - unit tests

--- src/app/wizard/results.component.spec.ts
+++ src/app/wizard/results.component.spec.ts
@@ -6,6 +6,7 @@ import {ResultsComponent} from './results.component';
 describe('ResultsComponent', () => {
   let component: ResultsComponent;
   let fixture: ComponentFixture<ResultsComponent>;
+  let BT_200_160_RESPONSE: any;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -14,7 +15,34 @@ describe('ResultsComponent', () => {
       .compileComponents();
     fixture = TestBed.createComponent(ResultsComponent);
     component = fixture.componentInstance;
+    BT_200_160_RESPONSE = {
+      wall_sleeve_type: 'WALL_SLEEVE_TYPE_OTHER',
+      inner_diameter: 200,
+      carrier_pipe_type: 'CARRIER_PIPE_TYPE_OTHER',
+      outer_diameter: 160,
+      clamping_ring: 20,
+      productLoc: 'Link-Seal S316',
+      context: 'PSI',
+      solutions: [{
+        mdls: 183.02818455568,
+        adls: 201.02818455568,
+        idls: 165.02818455568,
+        lspro: 10,
+        artNr: '2-025-00187',
+        rfArtNr: null,
+        nls: 10,
+        gespannteDicke: 22.5,
+        ungespannteDicke: 18,
+        type: 310,
+        adlsWarningClass: 'adlsWarningClass'
+      }],
+      sets: [],
+      // KR added for compilation
+      error: null,
+      notices: [] // add 'DEVIATIONS' to show coloured deviation text at end
+    };
     component.resultForConfiguration = BT_200_160_RESPONSE;
+
   });
 
   it('should create', () => {
@@ -62,31 +90,33 @@ describe('ResultsComponent', () => {
     const eleRed = fixture.debugElement.query(By.css('.outer-diameter-mm.red'));
     expect(eleRed).toBeTruthy();
   });
-});
+  describe('extended information', () => {
+    it('"Required Wall Thickness" not displayed', () => {
+      expectInfoExtensionsFalsy('mmRequiredWallThickness');
+    });
+    it('"Required Wall Thickness displayed"', () => {
+      component.resultForConfiguration.solutions[0].mmRequiredWallThickness = 100;
+      expectInfoExtensionsTruthy('mmRequiredWallThickness');
+    });
+    it('"Recommended Sleeve Length" not displayed', () => {
+      expectInfoExtensionsFalsy('mmRecommendedSleeveLength');
+    });
+    it('"Recommended Sleeve Length" displayed', () => {
+      component.resultForConfiguration.solutions[0].mmRecommendedSleeveLength = 150;
+      expectInfoExtensionsTruthy('mmRecommendedSleeveLength');
+    });
+  });
 
-export const BT_200_160_RESPONSE = {
-  wall_sleeve_type: 'WALL_SLEEVE_TYPE_OTHER',
-  inner_diameter: 200,
-  carrier_pipe_type: 'CARRIER_PIPE_TYPE_OTHER',
-  outer_diameter: 160,
-  clamping_ring: 20,
-  productLoc: 'Link-Seal S316',
-  context: 'PSI',
-  solutions: [{
-    mdls: 183.02818455568,
-    adls: 201.02818455568,
-    idls: 165.02818455568,
-    lspro: 10,
-    artNr: '2-025-00187',
-    rfArtNr: null,
-    nls: 10,
-    gespannteDicke: 22.5,
-    ungespannteDicke: 18,
-    type: 310,
-    adlsWarningClass: 'adlsWarningClass'
-  }],
-  sets: [],
-  // KR added for compilation
-  error: null,
-  notices: [] // add 'DEVIATIONS' to show coloured deviation text at end
-};
+  function expectInfoExtensionsFalsy(extProperty: string): void {
+    expect(component.resultForConfiguration.solutions[0].hasOwnProperty(extProperty)).toBeFalsy();
+    fixture.detectChanges();
+    const ele = fixture.debugElement.query(By.css(`[data-test-${extProperty}]`));
+    expect(ele).toBeFalsy();
+  }
+
+  function expectInfoExtensionsTruthy(extProperty: string): void {
+    expect(component.resultForConfiguration.solutions[0].hasOwnProperty(extProperty)).toBeTruthy();
+    fixture.detectChanges();
+    expect(fixture.debugElement.query(By.css(`[data-test-${extProperty}]`))).toBeTruthy();
+  }
+});
